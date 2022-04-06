0434dc8b5dad6dd0a5d9e901a27f7147c1a9a8ec
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2022-04-06 16:31:18 -0400

    Update rw_visual.py
    Just played around with a few settings to experiment

--- rw_visual.py
+++ rw_visual.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as mplplt
 from random_walk import RandomWalk
 
 # Make a random walk
-rw = RandomWalk(5000)
+rw = RandomWalk(50_000)
 rw.fill_walk()
 
 # Plot the points in the walk
 mplplt.style.use('classic')
 fig,ax = mplplt.subplots(figsize = (15,9))
 point_numbers = range(rw.num_points)
-# ax.scatter(rw.x_values, rw.y_values, c = point_numbers, cmap = mplplt.cm.Blues, edgecolors = 'none', s = 1)
-ax.plot(rw.x_values, rw.y_values, linewidth = 1)
+ax.scatter(rw.x_values, rw.y_values, c = point_numbers, cmap = mplplt.cm.Blues, edgecolors = 'none', s = 1)
+# ax.plot(rw.x_values, rw.y_values, linewidth = 1)
 
 # Emphasize the first and last points
 ax.scatter(0,0, c ='green', edgecolors = 'none', s = 100)
