3dd6f2905a74a89e32404f4552f5b8ca332f6a5f
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2022-04-07 16:30:08 -0400

    List comprehension
    Switched from for loops to list comprehension

--- die_multiplication.py
+++ die_multiplication.py
@@ -8,17 +8,11 @@ die_1 = Die()
 die_2 = Die()
 
 # Make some rolls and store results in a list
-results = []
-for roll_num in range(50_000):
-	result = die_1.roll() * die_2.roll()
-	results.append(result)
+results = [die_1.roll() * die_2.roll() for roll_num in range(50_000)]
 
 # Analyze results
-frequencies = []
 max_result = die_1.num_sides * die_2.num_sides
-for value in range(1, max_result+1):
-	frequency = results.count(value)
-	frequencies.append(frequency)
+frequencies = [results.count(value) for value in range(1, max_result+1)]
 
 # Visualize results
 
