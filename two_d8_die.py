d261089c1c85b3bb65a426b0d5f22bef56767095
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2022-04-07 16:26:18 -0400

    Update two_d8_die.py

--- two_d8_die.py
+++ two_d8_die.py
@@ -13,9 +13,6 @@ results = [die_1.roll() + die_2.roll() for roll_num in range(10_000_000)]
 # Analyze the results of the data
 max_result = die_1.num_sides + die_2.num_sides
 frequencies = [results.count(value) for value in range(2, max_result+1)]
-# for value in range(2, max_result+1):
-# 	frequency = results.count(value)
-# 	frequencies.append(frequency)
 
 # Visualize the results
 x_values = list(range(2, max_result+1))
