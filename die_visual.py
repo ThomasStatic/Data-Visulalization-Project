c8bb276fb781427498ce365e4702265020a40407
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2022-04-07 16:22:03 -0400

    Dice List Comprehension
    Swapped from for loops to list comprehension

--- die_visual.py
+++ die_visual.py
@@ -7,7 +7,6 @@ from die import Die
 die_1 = Die()
 die_2 = Die()
 
-
 # Make some results and store the results in a list
 roll_num = range(1000)
 results = [die_1.roll() + die_2.roll() for x in roll_num]
@@ -16,9 +15,6 @@ results = [die_1.roll() + die_2.roll() for x in roll_num]
 max_result = die_1.num_sides + die_2.num_sides
 value_range = range(2, max_result + 1)
 frequencies = [results.count(value) for value in value_range]
-# for value in range(2, max_result + 1):
-# 	frequency = results.count(value)
-# 	frequencies.append(frequency)
 
 # Visualize the results
 x_values = list(range(2, max_result+1))
