8d48b9ba510f8d8895394b8158cd16320437693a
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2022-04-07 16:28:12 -0400

    List Comprehension
    Switched from for loops to list comprehension

--- 3_die_6_visualization.py
+++ 3_die_6_visualization.py
@@ -9,17 +9,14 @@ die_2 = Die()
 die_3 = Die()
 
 # Make some rolls and store the results in a list
-results = []
-for num_roll in range(50_000):
-	result = die_1.roll() + die_2.roll() + die_3.roll()
-	results.append(result)
+results = [die_1.roll() + die_2.roll() + die_3.roll() for num_roll in range(50_000)]
 
 # Analyze the results
-frequencies = []
 max_result = die_1.num_sides + die_2.num_sides + die_3.num_sides
-for value in range(3, max_result+1):
-	frequency = results.count(value)
-	frequencies.append(frequency)
+frequencies = [results.count(value) for value in range(3, max_result+1)]
+# for value in range(3, max_result+1):
+# 	frequency = results.count(value)
+# 	frequencies.append(frequency)
 
 # Visualize data
 x_values = list(range(3, max_result+1))
